10e21fe21fb38b06b99d40932e356c78942792bf
Hassan Refaat <hassan.refaat.dev@gmail.com>
2023-07-20 11:15:15 +0300

    Initial commit

---
+++ src/main/java/io/nerd/springredissession/controller/HomeController.java
@@ -0,0 +1,31 @@
+package io.nerd.springredissession.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class HomeController {
+
+    private final String HOME_VIEW_COUNT = "HOME_VIEW_COUNT";
+
+    @GetMapping("/")
+    public String home(Principal principal, HttpSession session) {
+        incrementCount(session, HOME_VIEW_COUNT);
+        return "hello " + principal.getName();
+    }
+
+    @GetMapping("/count")
+    public String count(HttpSession session) {
+        return "HOME_VIEW_COUNT: " + session.getAttribute(HOME_VIEW_COUNT);
+    }
+
+    private void incrementCount(HttpSession session, String attr) {
+        var homeViewCount = session.getAttribute(attr) == null ? 0 : (Integer) session.getAttribute(attr);
+        session.setAttribute(attr, homeViewCount += 1);
+    }
+
+
+}
---
+++ src/test/java/io/nerd/springredissession/SpringRedisSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nerd.springredissession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisSessionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
